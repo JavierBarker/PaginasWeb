bffc76fccc1c55ba2f2067c1b4f201dd3e00fcf8
Javier Barker <hernandez.javier2003@gmail.com>
2022-07-31 20:18:10 -0600

    add: web page

---
+++ noticias/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu_btn').addEventListener('click', () =>{
+    document.querySelector('.nav_menu').classList.toggle('show')
+});
+
+
+ScrollReveal().reveal('.nav_main', {delay: 500});
+ScrollReveal().reveal('.showcase', {delay: 500});
+ScrollReveal().reveal('.cards_news', {delay: 500});
+ScrollReveal().reveal('.cards_banner_one', {delay: 500});
+ScrollReveal().reveal('.cards_news', {delay: 500});
+ScrollReveal().reveal('.cards_banner_two', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
+ScrollReveal().reveal('.footer_links', {delay: 500});
